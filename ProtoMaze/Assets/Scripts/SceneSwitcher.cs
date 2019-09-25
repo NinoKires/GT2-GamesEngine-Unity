90510fb780ae2c045f1bfc23a39c710d72c1e763
Ninoslav Kjireski <nino.kireski@googlemail.com>
2019-09-25 18:06:52 +0200

    1

---
+++ ProtoMaze/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    void OnGenNewMazeClicked()
+    {
+        SceneManager.LoadScene("gameTemp");
+    }
+}
